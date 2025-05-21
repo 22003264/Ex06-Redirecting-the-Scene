12c7fc3a50a2c2f7785e27322170ed815b0dfa91
22003264 <reddysirisha0501@gmail.com>
2025-05-21 08:22:29 +0530

    added

--- Assets/coding.cs
+++ Assets/coding.cs
@@ -1,47 +1,3 @@
-# Ex06-Redirect the Scene
-
-## Aim :
-
-To Redirecting the scene in the unity engine.
-
-## Algorithm :
-
-### Step 1 :
-
-To open the unity engine.
-
-### Step 2 :
-
-Create a new 3D project.
-
-### Step 3 :
-
-Create plane and name it as ground and create cube and name it as player.
-
-### Step 4 :
-
-Add WinText in Hierarchy.
-
-### Step 5 :
-
-Create a C# Script and name it as playercontroller and add the script to player.
-
-### Step 6 :
-
-Use the R button to change the level 2
-
-### Step 7 :
-
-Print the Output and end the program.
-
-## Program :
-
-### DEVELOPED BY:Onteddu Sirisha
-### REGISTER NUMBER : 212222230103
-
-## CUBE PLAYER :
-
-```
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -77,11 +33,3 @@ public class Coding : MonoBehaviour
         }
     }
 }
-```
-## Output :
-
-
-![alt text](image.png)
-## Result :
-
-The above C# coding is successfully redirecting the scene in the unity engine.
